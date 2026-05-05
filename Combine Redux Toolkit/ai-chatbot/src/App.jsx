363837892aae80a7252710d26d990d2f4d10a98a
BenzHoang <benzminh2003@gmail.com>
2026-05-05 22:56:24 +0700

    Add Header UI

--- Combine Redux Toolkit/ai-chatbot/src/App.jsx
+++ Combine Redux Toolkit/ai-chatbot/src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
+import { useState } from "react";
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="flex flex-col h-screen">
+      <Header toggleDarkMode={toggleDarkMode} darkMode={isDarkMode}/>
     </div>
   );
 };
