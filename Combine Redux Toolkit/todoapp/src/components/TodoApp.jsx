074f199691648b693efe547a6337a7b9ee381d3c
BenzHoang <benzminh2003@gmail.com>
2026-06-23 23:08:32 +0700

    Add Todo App Component #2

--- Combine Redux Toolkit/todoapp/src/components/TodoApp.jsx
+++ Combine Redux Toolkit/todoapp/src/components/TodoApp.jsx
@@ -1,3 +1,7 @@
+import { CheckCircle2, Circle, Filter, Plus, Trash2 } from "lucide-react";
+import TodoFilters from "./TodoFilters";
+import TodoForm from "./TodoForm";
+
 const TodoApp = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-100 via-gray-200 to-gray-300 px-4 py-8">
@@ -51,7 +55,69 @@ const TodoApp = () => {
         </div>
 
         {/* Main Todo Container */}
-        <div className=""></div>
+        <div
+          className="bg-white/90 backdrop-blur-sm rounded-b-2xl border border-gray-200
+        shadow-lg overflow-hidden"
+        >
+          {/* Active Bar */}
+          <div className="p-6 border-b border-gray-300">
+            <div className="flex items-center justify-between mb-4">
+              <button
+                className="flex items-center gap-3 bg-blue-600 hover:bg-blue-700
+              text-white px-4 py-2 rounded-lg transition-colors duration-200 font-medium
+              cursor-pointer"
+              >
+                <Plus size={20} /> Add Todo
+              </button>
+
+              {/* Clear and Delete Buttons */}
+              <div className="flex items-center gap-2">
+                <button
+                  className="flex items-center gap-3 text-red-600 hover:text-red-700
+                px-3 py-2 rounded-lg hover:bg-red-50 transition-colors duration-200 text-sm"
+                >
+                  <Trash2 size={16} />
+                  Clear Completed
+                </button>
+                <button
+                  className="flex items-center gap-3 text-green-600 hover:text-green-700
+                px-3 py-2 rounded-lg hover:bg-green-50 transition-colors duration-200 text-sm"
+                >
+                  <CheckCircle2 size={16} />
+                  Mark All Completed
+                </button>
+              </div>
+            </div>
+
+            {/* Todo Filter */}
+            <TodoFilters />
+          </div>
+          {/* Todo Form */}
+          <div className="p-6 border-b border-gray-300 bg-gray-100">
+            <TodoForm />
+          </div>
+
+          {/* Todo List */}
+          <div className="max-h-96 overflow-y-auto">
+            <div className="p-12 text-center">
+              <div className="text-gray-600">
+                <Circle size={48} className="mx-auto mb-4 opacity-50" />
+                <p className="text-lg font-medium mb-2 text-gray-800">
+                  No Todos Yet
+                </p>
+                <p>Add your first todo to get started!</p>
+              </div>
+
+              {/* Conditional Rendering */}
+              <div className="text-gray-600">
+                <Filter size={48} className="mx-auto mb-4 opacity-50" />
+                <p className="text-lg font-medium mb-2 text-gray-800">
+                  No Filter Todos
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
